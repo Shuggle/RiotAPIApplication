cfb558c7fd7d40c7629cf84ad49d664aea6a40a4
Julien Alric <julien211195@gmail.com>
2018-03-12 00:01:33 +0100

    Re add Resource file

--- RiotAPIApplication/Properties/Resource.Designer.cs
+++ RiotAPIApplication/Properties/Resource.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RiotAPIApplication {
+namespace RiotAPIApplication.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace RiotAPIApplication {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RiotAPIApplication.Resource", typeof(Resource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RiotAPIApplication.Properties.Resource", typeof(Resource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
