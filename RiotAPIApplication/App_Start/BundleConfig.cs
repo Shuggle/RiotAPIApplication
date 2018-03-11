59e3306ed09f708bf90e9f7662b2e0982be48a42
PÃ?PIN JÃ©rÃ©my <informatique3@pesage-act.com>
2018-03-11 22:25:04 +0100

    Modification du layout et de la page d'acceuil pour supporter materialize

--- RiotAPIApplication/App_Start/BundleConfig.cs
+++ RiotAPIApplication/App_Start/BundleConfig.cs
@@ -19,13 +19,13 @@ namespace RiotAPIApplication
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/materialize.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/materialize").Include(
+                "~/Content/materialize.js"
+                ));
         }
     }
 }
